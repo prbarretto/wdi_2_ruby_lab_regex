3aba62fef7c1becfee2d4bb1368ec49229d92004
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-14 16:20:00 -0500

    regexp so far

--- main.rb
+++ main.rb
@@ -1,26 +1,29 @@
 require_relative 'source'
 require 'pry'
 
-def valid_ip_addresses 
-end 
-
-def valid_mit_ip_addresses 
+def valid_ip_addresses(source_text)
+	source_text.scan(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
 end
 
-def non_mit_ip_addresses 
-end 
-
-def valid_phone_numbers 
+def valid_mit_ip_addresses(source_text)
+	source_text.scan(/18\.\d{1,3}\.\d{1,3}\.\d{1,3}/)
 end
 
-def area_codes 
+def non_mit_ip_addresses(source_text)
+	valid_ip_addresses - valid_mit_ip_addresses
 end
 
-def email_addresses 
+def valid_phone_numbers
 end
 
-def zip_codes 
+def area_codes
 end
 
-def hex_colors 
+def email_addresses
+end
+
+def zip_codes
+end
+
+def hex_colors
 end
